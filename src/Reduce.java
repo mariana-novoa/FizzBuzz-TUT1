64d5c9402817ffc2d67cdab2020be00e2480d002
novoarea <mariana.novoa@mail.utoronto.ca>
2024-09-12 11:27:30 -0400

    reduce function

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+
+        int count = 0;
+        int n = 100;
+        while (n != 0){
+            if (n % 2 == 0){
+                count++;
+                n = n / 2;
+            } else {
+                count++;
+                n = n - 1;
+            }
+        }
+        System.out.println(count);
+    }
 }
